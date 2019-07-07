3fcac02376bc178565a948b535b3a33da11ff814
Stypox <stypox@pm.me>
2019-07-07 14:32:18 +0200

    Add Tokenizer along with tests

---
+++ sentences_compiler/src/main/java/com/stypox/sentences_compiler/lexer/Tokenizer.java
@@ -0,0 +1,94 @@
+package com.stypox.sentences_compiler.lexer;
+
+import com.stypox.sentences_compiler.CompilerError;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class Tokenizer {
+    private InputStreamReader input;
+    private int line = 1, column = 0;
+
+    Tokenizer(InputStream inputStream) {
+        this.input = new InputStreamReader(inputStream, Charset.forName("unicode"));
+    }
+
+    private String getCh() throws IOException {
+        int intCh = input.read();
+        if (intCh == -1) {
+            return "";
+        }
+
+        ++column;
+        return new String(Character.toChars(intCh));
+    }
+    private Token.Type updateWordType(Token.Type currentWordType, String ch) {
+        if (currentWordType == Token.Type.lettersPlusOther || isOtherValid(ch)) {
+            return Token.Type.lettersPlusOther;
+        } else {
+            return Token.Type.letters;
+        }
+    }
+
+    private boolean isSpace(String ch) {
+        return Character.isWhitespace(ch.codePointAt(0)) && !ch.equals("\n");
+    }
+    private boolean isLetter(String ch) {
+        return Character.isLetter(ch.codePointAt(0));
+    }
+    private boolean isOtherValid(String ch) {
+        return Character.isDigit(ch.codePointAt(0)) || ch.equals("_");
+    }
+    private boolean isGrammar(String ch) {
+        switch (ch) {
+            case ":": case ";": case "|": case ".": case "<": case ">": case "(": case ")":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public TokenStream tokenize() throws IOException, CompilerError {
+        TokenStream ts = new TokenStream();
+        String ch = getCh();
+
+        while (!ch.isEmpty()) {
+            if (isLetter(ch) || isOtherValid(ch)) {
+                StringBuilder word = new StringBuilder(ch);
+                Token.Type wordType = Token.Type.letters;
+                int firstCharCol = column;
+
+                while (true) {
+                    wordType = updateWordType(wordType, ch);
+                    ch = getCh();
+                    if (isLetter(ch) || isOtherValid(ch)) {
+                        word.append(ch);
+                    } else {
+                        break;
+                    }
+                }
+
+                ts.push(new Token(wordType, word.toString(), line, firstCharCol));
+            } else if (isGrammar(ch)) {
+                ts.push(new Token(Token.Type.grammar, ch, line, column));
+                ch = getCh();
+            } else if (ch.equals("\n")) {
+                ++line;
+                column = 0;
+                ch = getCh();
+            } else if (ch.equals("#")) {
+                while (!ch.equals("\n") && !ch.isEmpty()) {
+                    ch = getCh();
+                }
+            } else if (isSpace(ch)) {
+                ch = getCh();
+            } else {
+                throw new CompilerError(CompilerError.Type.invalidCharacter, ch, column, line, "");
+            }
+        }
+
+        return ts;
+    }
+}
---
+++ sentences_compiler/src/test/java/com/stypox/sentences_compiler/lexer/TokenizerTest.java
@@ -0,0 +1,78 @@
+package com.stypox.sentences_compiler.lexer;
+
+import com.stypox.sentences_compiler.CompilerError;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.*;
+
+public class TokenizerTest {
+
+    private static Tokenizer fromString(String s) {
+        InputStream stream = new ByteArrayInputStream(s.getBytes(Charset.forName("unicode")));
+        return new Tokenizer(stream);
+    }
+    private static TokenStream getTokens(String s) throws IOException, CompilerError {
+        return fromString(s).tokenize();
+    }
+    private static void assertTokenEqualTo(Token token, Token.Type type, String value, int line, int column) {
+        assertTrue(token.isType(type));
+        assertTrue(token.isValue(value));
+        assertEquals(token.getLine(), line);
+        assertEquals(token.getColumn(), column);
+    }
+    private static void assertInvalid(String input, CompilerError.Type errorType, int errorLine, int errorColumn, String errorMustContain) throws IOException {
+        try {
+            getTokens(input);
+            fail("No error thrown with invalid input");
+        } catch (CompilerError compilerError) {
+            String message = compilerError.getMessage();
+            assertTrue(message.contains(errorType.toString()));
+            assertTrue(message.contains(String.valueOf(errorLine)));
+            assertTrue(message.contains(String.valueOf(errorColumn)));
+            assertTrue(message.contains(errorMustContain));
+        }
+    }
+
+    @Test
+    public void testEmptyInput() throws IOException, CompilerError {
+        assertTrue(getTokens("").get().isEmpty());
+        assertTrue(getTokens("\n").get().isEmpty());
+        assertTrue(getTokens("  \t  \n\t\t\n  ").get().isEmpty());
+        assertTrue(getTokens("    # hello: # ## | | ; world   \n#").get().isEmpty());
+    }
+
+    @Test
+    public void testValidInput() throws IOException, CompilerError {
+        TokenStream tokens = getTokens("cat_name1:     # hello world  \n" +
+                "  <sent_name1> hello      |hi|(bye bye);       \n");
+        assertTrue(tokens.get(-1).isEmpty());
+        assertTokenEqualTo(tokens.get(0),  Token.Type.lettersPlusOther, "cat_name1",  1,  1);
+        assertTokenEqualTo(tokens.get(1),  Token.Type.grammar,          ":",          1, 10);
+        assertTokenEqualTo(tokens.get(2),  Token.Type.grammar,          "<",          2,  3);
+        assertTokenEqualTo(tokens.get(3),  Token.Type.lettersPlusOther, "sent_name1", 2,  4);
+        assertTokenEqualTo(tokens.get(4),  Token.Type.grammar,          ">",          2, 14);
+        assertTokenEqualTo(tokens.get(5),  Token.Type.letters,          "hello",      2, 16);
+        assertTokenEqualTo(tokens.get(6),  Token.Type.grammar,          "|",          2, 27);
+        assertTokenEqualTo(tokens.get(7),  Token.Type.letters,          "hi",         2, 28);
+        assertTokenEqualTo(tokens.get(8),  Token.Type.grammar,          "|",          2, 30);
+        assertTokenEqualTo(tokens.get(9),  Token.Type.grammar,          "(",          2, 31);
+        assertTokenEqualTo(tokens.get(10), Token.Type.letters,          "bye",        2, 32);
+        assertTokenEqualTo(tokens.get(11), Token.Type.letters,          "bye",        2, 36);
+        assertTokenEqualTo(tokens.get(12), Token.Type.grammar,          ")",          2, 39);
+        assertTokenEqualTo(tokens.get(13), Token.Type.grammar,          ";",          2, 40);
+        assertTrue(tokens.get(14).isEmpty());
+    }
+
+    @Test
+    public void testInvalidInput() throws IOException {
+        assertInvalid("+",                      CompilerError.Type.invalidCharacter, 1, 1, "+");
+        assertInvalid("   \n  *\n\n+",          CompilerError.Type.invalidCharacter, 2, 3, "*");
+        assertInvalid("<<><\n\t>(\n([)):;)):(", CompilerError.Type.invalidCharacter, 3, 2, "[");
+    }
+}
