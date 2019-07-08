40d8cd58a011610cff10f7d0b5067da4f6225011
Stypox <stypox@pm.me>
2019-07-08 14:12:15 +0200

    Add test for multiplyArray

---
+++ sentences_compiler/src/test/java/com/stypox/sentences_compiler/util/UnfoldingUtilsTest.java
@@ -0,0 +1,28 @@
+package com.stypox.sentences_compiler.util;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class UnfoldingUtilsTest {
+
+    @Test
+    public void testMultiplyArray() {
+        ArrayList<ArrayList<String>> strings = new ArrayList<ArrayList<String>>() {{
+            add(new ArrayList<String>() {{ add("a"); }});
+            add(new ArrayList<String>() {{ add("b"); }});
+        }};
+        ArrayList<ArrayList<String>> strings2 = UnfoldingUtils.multiplyArray(strings, 2);
+
+        // also check that arrays are all unique, i.e. there are no equal references
+        strings2.get(0).add("A");
+        assertSame(strings2.size(), 2 * strings.size());
+        assertArrayEquals(new String[]{"a", "A"}, strings2.get(0).toArray());
+        assertArrayEquals(new String[]{"b"},      strings2.get(1).toArray());
+        assertArrayEquals(new String[]{"a"},      strings2.get(2).toArray());
+        assertArrayEquals(new String[]{"b"},      strings2.get(3).toArray());
+    }
+}
