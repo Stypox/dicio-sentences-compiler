8aed20ebde2ce852156068a80f9ee2caa6b21d45
Stypox <stypox@pm.me>
2019-07-08 14:51:04 +0200

    Implement Section and Sentence constructs

--- sentences_compiler/src/main/java/com/stypox/sentences_compiler/parser/construct/Section.java
+++ sentences_compiler/src/main/java/com/stypox/sentences_compiler/parser/construct/Section.java
@@ -1,4 +1,19 @@
 package com.stypox.sentences_compiler.parser.construct;
 
+import java.util.ArrayList;
+
 public class Section {
+    private String sectionId;
+    private ArrayList<Sentence> sentences;
+
+    Section() {
+        sentences = new ArrayList<>();
+    }
+
+    public void setSectionId(String sectionId) {
+        this.sectionId = sectionId;
+    }
+    public void addSentence(Sentence sentences) {
+        this.sentences.add(sentences);
+    }
 }
--- sentences_compiler/src/main/java/com/stypox/sentences_compiler/parser/construct/Sentence.java
+++ sentences_compiler/src/main/java/com/stypox/sentences_compiler/parser/construct/Sentence.java
@@ -1,4 +1,15 @@
 package com.stypox.sentences_compiler.parser.construct;
 
+import java.util.ArrayList;
+
 public class Sentence {
+    private String sentenceId;
+    private SentenceConstructList sentenceConstructs;
+
+    public void setSentenceId(String sentenceId) {
+        this.sentenceId = sentenceId;
+    }
+    public void setSentenceConstructs(SentenceConstructList sentenceConstructs) {
+        this.sentenceConstructs = sentenceConstructs;
+    }
 }
